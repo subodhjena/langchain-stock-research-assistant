b097078b1aa76f4f7d5320f622998f281e931205
Subodh Jena <jenasubodh@gmail.com>
2023-05-02 19:03:47 +0530

    Basic form

--- app.py
+++ app.py
@@ -5,12 +5,23 @@ from core.assistant import run
 
 load_dotenv()
 
-symbol = "DMART"
-st.title("Stock market research assistant")
-st_text_input = st.text_input("Ask anything")
+symbol = ""
 
-if st_text_input:
-    result = run(symbol, st_text_input)
-    st.write(result['answer'])
-else:
-    run(symbol, "")  # this is just to trigger the data refresh if needed
+# Sidebar
+st.sidebar.write("## Analyze any stock")
+st.sidebar.write(
+    "Enter a stock name and press enter (Currently only works with indian stocks)")
+stock_input = st.sidebar.text_input("Enter a stock name")
+if stock_input:
+    symbol = stock_input.upper()
+
+# Content Page
+if symbol:
+    st.title(symbol)
+    st_text_input = st.text_input("Ask anything")
+
+    if st_text_input:
+        result = run(symbol, st_text_input)
+        st.write(result['answer'])
+    else:
+        run(symbol, "")  # this is just to trigger the data refresh if needed
